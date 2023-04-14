305226562c421b49d26262b7096a65ef78427a3a
Mr. 3rd <97487150+Mr-3rd@users.noreply.github.com>
2023-04-13 23:49:04 -0500

    Second Upload

--- Example-API/APICall.js
+++ Example-API/APICall.js
@@ -1,20 +1,55 @@
 var axios = require('axios');
 
-var config = {
-    method: 'get',
-    url: 'https://alpha-api.usbank.com/innovation/bank-node/reference/v1/products',
-    headers: {
-        'Accept': 'application/json',
-    },
-    auth: {
-        username: 'uXK5uhtGqOxcdsJBzWW2z0HYUGpsUlse',
-        password: 'yYxzZL68jIGFa4OE'
+function getAllMCCData () {
+    var config = {
+        method: 'get',
+        url: 'https://alpha-api.usbank.com/innovation/bank-node/reference/v1/products',
+        headers: {
+            'Accept': 'application/json',
+        },
+        auth: {
+            username: 'uXK5uhtGqOxcdsJBzWW2z0HYUGpsUlse',
+            password: 'yYxzZL68jIGFa4OE'
+        }
     }
+
+
+    axios(config).then(function (response) {
+        return JSON.stringify(response.data);
+    }).catch(function (error) {
+        console.log(error);
+    })
 }
 
 
-axios(config).then(function (response) {
-    console.log(JSON.stringify(response.data));
-}).catch(function (error) {
-    console.log(error);
-})
+function getCCTransactionHistory() {
+
+    var accountID = 935121592917;
+
+    var config = {
+        method: 'get',
+        url: 'https://alpha-api.usbank.com/innovation/bank-node/customer-accounts/v1/account/' + accountID + '/trans/full',
+        headers: {
+            'Accept': 'application/json',
+        },
+        auth: {
+            username: 'uXK5uhtGqOxcdsJBzWW2z0HYUGpsUlse',
+            password: 'yYxzZL68jIGFa4OE'
+        }
+    }
+
+    axios(config).then(function (response) {
+        return JSON.stringify(response.data);
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+
+function addMCCData() {
+    let allMCCData = getAllMCCData();
+    let allTransData = getCCTransactionHistory();
+
+    console.log(allMCCData)
+}
+
+addMCCData()
---
+++ Example.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+var config = {
+    method: 'get',
+    url: 'https://alpha-api.usbank.com/innovation/bank-node/reference/v1/products',
+    headers: {
+        'Accept': 'application/json',
+    },
+    auth: {
+        username: 'uXK5uhtGqOxcdsJBzWW2z0HYUGpsUlse',
+        password: 'yYxzZL68jIGFa4OE'
+    }
+}
+
+
+axios(config).then(function (response) {
+    console.log(JSON.stringify(response.data));
+}).catch(function (error) {
+    console.log(error);
+})
